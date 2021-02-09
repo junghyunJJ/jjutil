1048608c79e7939cb5aedee3b44f2b8fe58ae91c
junghyunJJ <swordbreadjj@gmail.com>
2021-02-09 13:17:43 -0800

    ccc

--- R/mmc_jj.R
+++ R/mmc_jj.R
@@ -685,3 +685,95 @@ mmccor2 <- function(x, y=NULL, ngrids=100, llim=-10, ulim=10, esp=1e-10, core=1)
   }
   return(rs)
 }
+
+
+
+cor_lmmite <- function(x, use = "p"){
+  y=NULL;ngrids=100;llim=-10;ulim=10;esp=1e-10
+  # x<-t(x)
+  K = mmc.ncov(x)
+  # Currently there are issues with using other covariates so I am only regressing out the mean
+  # I'm looking into this issue
+  #mmc.corsym <- function(x, K, X0 = NULL, ngrids=100, llim=-10, ulim=10, esp=1e-10){
+  #if ( is.null(X0) ) {
+  #  X0 <- matrix(1,ncol(x),1)
+  #}
+
+  X0 <- matrix(1,ncol(x),1)
+  X01 = X0
+  X02 = X0
+
+  N = nrow(x)
+
+  q01 <- ncol(X01)
+  q11 <- q01 + 1
+  q02 <- ncol(X02)
+  q12 <- q02 + 1
+
+  rs <- matrix(NA,nrow=N,ncol=N)
+
+  for(i in 1:N){
+    tt <- as.character(Sys.time())
+    cat(i,"/",nrow(x),": ",tt,"\n",sep = "")
+    rs[i,i] <- 1
+    if(i == N){ break }
+    for(j in (i+1):N){
+      # cat(j,"\n",sep = "")
+
+      vids <- intersect(which(!is.na(x[i,])),which(!is.na(x[j,])))
+      nv <- length(vids)
+
+      # Direction 1
+      X1 <- cbind(X01[vids,,drop=FALSE],x[i,vids])
+      if (det(crossprod(X1,X1)) == 0){ next; }
+      # eig.R1 = emma.eigen.R.wo.Z(K[vids,vids],X1)
+      # REMLE <- emma.REMLE(x[j,vids],X1,K[vids,vids],NULL,ngrids,llim,ulim,esp,eig.R1)
+      # vgs1 <- REMLE$vg
+      # ves1 <- REMLE$ve
+      #
+      e <- lmmlite::eigen_rotation(K, x[j,vids], X1 )
+      out <- lmmlite::fitLMM(e$Kva, e$y, e$X, tol = 1e-10)
+      vgs1 <- out$sigmasq_g
+      ves1 <- out$sigmasq_e
+
+      # Direction 2
+      X2 <- cbind(X02[vids,,drop=FALSE],x[j,vids])
+      if (det(crossprod(X2,X2)) == 0){ next; }
+      # eig.R1 = emma.eigen.R.wo.Z(K[vids,vids],X2)
+      # REMLE <- emma.REMLE(x[i,vids],X2,K[vids,vids],NULL,ngrids,llim,ulim,esp,eig.R1)
+      # vgs2 <- REMLE$vg
+      # ves2 <- REMLE$ve
+      #
+      e <- lmmlite::eigen_rotation(K, x[i,vids], X2 )
+      out <- lmmlite::fitLMM(e$Kva, e$y, e$X, tol = 1e-10)
+      vgs2 <- out$sigmasq_g
+      ves2 <- out$sigmasq_e
+
+      #cat(vgs1,",",vgs2,"\n")
+      #cat(ves1,",",ves2,"\n\n")
+
+      sigmaHat1 = vgs1*K[vids,vids] + ves1*diag(nv)
+      sigmaHat2 = vgs2*K[vids,vids] + ves2*diag(nv)
+      sigmaHat1Inv = solve(sigmaHat1)
+      sigmaHat2Inv = solve(sigmaHat2)
+      sigmaHat1InvChol = chol(sigmaHat1Inv)
+      sigmaHat2InvChol = chol(sigmaHat2Inv)
+
+      y1adj = matrix(x[j,vids] - mean(x[j,vids]),ncol=1)
+      y2adj = matrix(x[i,vids] - mean(x[i,vids]),ncol=1)
+      #y1adj = matrix(matrix(x[j,vids],ncol=1) - (X1 %*% matrix(beta1,ncol=1)),ncol=1)
+      #y2adj = matrix(matrix(x[i,vids],ncol=1) - (X2 %*% matrix(beta2,ncol=1)),ncol=1)
+
+      y1adj.chol = sigmaHat1InvChol %*% y1adj
+      y2adj.chol = sigmaHat2InvChol %*% y2adj
+
+      var1 = t(y1adj.chol) %*% y1adj.chol
+      var2 = t(y2adj.chol) %*% y2adj.chol
+
+      rs[i,j] <- (t(y1adj.chol) %*% y2adj.chol) / sqrt(var1 * var2)
+      rs[j,i] <- rs[i,j]
+
+    }
+  }
+  return(rs)
+}
