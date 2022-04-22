07ebe689858d649b7a006ff0f38f379dcf80dbcc
junghyunJJ <swordbreadjj@gmail.com>
2022-04-22 13:35:39 -0700

    updated

--- R/list_to_gmt.R
+++ R/list_to_gmt.R
@@ -1,3 +1,18 @@
+# read.gmtfunctions in qusage r package
+read.gmt <- function(file){
+  if (!grepl("\\.gmt$", file)[1]) {
+    stop("Pathway information must be a .gmt file")
+  }
+  geneSetDB = readLines(file)
+  geneSetDB = strsplit(geneSetDB, "\t")
+  names(geneSetDB) = sapply(geneSetDB, "[", 1)
+  geneSetDB = lapply(geneSetDB, "[", -1:-2)
+  geneSetDB = lapply(geneSetDB, function(x) {
+    x[which(x != "")]
+  })
+  return(geneSetDB)
+}
+
 
 list_to_gmt <- function(listgeneset = listgeneset, file){
   if(is.null(file)) stop("'quote' must be 'TRUE', 'FALSE' or numeric")
