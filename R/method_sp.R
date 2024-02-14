dfb62d25ebd9548da7d559f0388ad130310c8f16
junghyunjj <swordbreadjj@gmial.com>
2024-02-14 14:28:25 -0800

    update "spatial_kernel"

--- R/method_sp.R
+++ R/method_sp.R
@@ -12,3 +12,47 @@ linear_kernel <- function(X) {
   K <- tcrossprod(as.matrix(X)) # K = np.dot(X, X.T)
   return(K / max(K))            # return K / K.max()
 }
+
+# dist_kernel <- function(X, l) {
+#   R2 <- as.matrix(dist(X) ** 2)
+#   K <- exp(-R2 / (2 * l ** 2))
+#   K
+# }
+
+spatial_kernel <- function(normalized_expr, coord, kerneltype = "gaussian", bandwidthtype = "SJ", bandwidth.set.by.user = NULL, sparseKernel = FALSE, sparseKernel_tol = 1e-20, sparseKernel_ncore = 1) {
+  # cal spatial Kernel using SpatialPCA r package
+
+  # normalized_expr: g x n (we used SCTransform)
+  # coord: n x 2 (i.e., x and y)
+
+  # The type of bandwidth to be used in Gaussian kernel,
+  #   1. "SJ" for Sheather & Jones (1991) method (usually used in small size datasets),
+  #   2. "Silverman" for Silverman's ‘rule of thumb’ method (1986)(usually used in large size datasets).
+
+  # scale expr data to calculate "bandwidth"
+  expr <- as.matrix(as.data.frame(t(scale(t(normalized_expr)))))
+
+  if (is.null(bandwidth.set.by.user)) {
+    bandwidth <- SpatialPCA::bandwidth_select(expr, method = bandwidthtype)
+    cat(paste("## cal bandwidth: ", bandwidth, "\n"), sep = "")
+  } else {
+    bandwidth <- bandwidth.set.by.user
+    cat(paste("## select bandwidth by user: ", bandwidth, "\n"), sep = "")
+  }
+
+  # scale coordinate data
+  location_normalized <- scale(coord)
+
+  if (sparseKernel == FALSE) {
+    cat(paste("## Calculating kernel matrix\n"))
+    kernelmat <- SpatialPCA::kernel_build(kerneltype = kerneltype, location = location_normalized, bandwidth = bandwidth)
+  } else if (sparseKernel == TRUE) {
+    cat(paste("## Calculating sparse kernel matrix\n"))
+    kernelmat <- SpatialPCA::kernel_build_sparse(
+      kerneltype = kerneltype,
+      location = location_normalized, bandwidth = bandwidth,
+      tol = sparseKernel_tol, ncores = sparseKernel_ncore
+    )
+  }
+  return(kernelmat)
+}
