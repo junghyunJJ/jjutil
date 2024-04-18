5f1e6dbe19067c2155433c60bdb44610dc73f671
junghyunjj <swordbreadjj@gmial.com>
2024-04-17 23:34:59 -0700

    update

--- R/utiles.R
+++ R/utiles.R
@@ -6,16 +6,19 @@ updatejjutil <- function(x){
   devtools::install_github("junghyunJJ/jjutil")
 }
 
-mouse_to_human <- function(mouse_ids, input = "ENSEMBL", target = "SYMBOL", horg = org.Hs.eg.db, morg = org.Mm.eg.db, orth = Orthology.eg.db) {
-  mouse_geneid <- AnnotationDbi::mapIds(morg, mouse_ids, "ENTREZID", input)
-  mapped <- AnnotationDbi::select(orth, mouse_geneid, "Homo_sapiens", "Mus_musculus")
-  human_target <- AnnotationDbi::select(horg, as.character(mapped[, 2]), target, "ENTREZID")
+mouse_to_human <- function(mouse_ids, input = "ENSEMBL", target = "SYMBOL") {
+  mouse_geneid <- AnnotationDbi::mapIds(org.Mm.eg.db::org.Mm.eg.db, mouse_ids, "ENTREZID", input)
+  mapped <- AnnotationDbi::select(Orthology.eg.db::Orthology.eg.db, mouse_geneid, "Homo_sapiens", "Mus_musculus")
+  human_target <- AnnotationDbi::select(org.Hs.eg.db::org.Hs.eg.db, as.character(mapped[, 2]), target, "ENTREZID")
   return(data.frame(mouse_input = mouse_ids, mapped, mouse_target = human_target[, 2]))
 }
 
-human_to_mouse <- function(human_ids, input = "ENSEMBL", target = "SYMBOL", horg = org.Hs.eg.db, morg = org.Mm.eg.db, orth = Orthology.eg.db) {
-  human_geneid <- AnnotationDbi::mapIds(horg, human_ids, "ENTREZID", input)
-  mapped <- AnnotationDbi::select(orth, human_geneid, "Mus_musculus", "Homo_sapiens")
-  mouse_target <- AnnotationDbi::select(morg, as.character(mapped[, 2]), target, "ENTREZID")
+human_to_mouse <- function(human_ids, input = "ENSEMBL", target = "SYMBOL") {
+  human_geneid <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db, human_ids, "ENTREZID", input)
+  mapped <- AnnotationDbi::select(Orthology.eg.db::Orthology.eg.db, human_geneid, "Mus_musculus", "Homo_sapiens")
+  mouse_target <- AnnotationDbi::select(org.Mm.eg.db::org.Mm.eg.db, as.character(mapped[, 2]), target, "ENTREZID")
   return(data.frame(human_input = human_ids, mapped, mouse_target = mouse_target[, 2]))
 }
+
+
+
