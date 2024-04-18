71a145395c9ac1b6fb633eea88c84ecadba360c0
junghyunjj <swordbreadjj@gmial.com>
2024-04-17 23:24:25 -0700

    update

--- R/utiles.R
+++ R/utiles.R
@@ -7,15 +7,15 @@ updatejjutil <- function(x){
 }
 
 mouse_to_human <- function(mouse_ids, input = "ENSEMBL", target = "SYMBOL", horg = org.Hs.eg.db, morg = org.Mm.eg.db, orth = Orthology.eg.db) {
-  mouse_geneid <- mapIds(morg, mouse_ids, "ENTREZID", input)
-  mapped <- select(orth, mouse_geneid, "Homo_sapiens", "Mus_musculus")
-  human_target <- select(horg, as.character(mapped[, 2]), target, "ENTREZID")
+  mouse_geneid <- AnnotationDbi::mapIds(morg, mouse_ids, "ENTREZID", input)
+  mapped <- AnnotationDbi::select(orth, mouse_geneid, "Homo_sapiens", "Mus_musculus")
+  human_target <- AnnotationDbi::select(horg, as.character(mapped[, 2]), target, "ENTREZID")
   return(data.frame(mouse_input = mouse_ids, mapped, mouse_target = human_target[, 2]))
 }
 
 human_to_mouse <- function(human_ids, input = "ENSEMBL", target = "SYMBOL", horg = org.Hs.eg.db, morg = org.Mm.eg.db, orth = Orthology.eg.db) {
-  human_geneid <- mapIds(horg, human_ids, "ENTREZID", input)
-  mapped <- select(orth, human_geneid, "Mus_musculus", "Homo_sapiens")
-  mouse_target <- select(morg, as.character(mapped[, 2]), target, "ENTREZID")
+  human_geneid <- AnnotationDbi::mapIds(horg, human_ids, "ENTREZID", input)
+  mapped <- AnnotationDbi::select(orth, human_geneid, "Mus_musculus", "Homo_sapiens")
+  mouse_target <- AnnotationDbi::select(morg, as.character(mapped[, 2]), target, "ENTREZID")
   return(data.frame(human_input = human_ids, mapped, mouse_target = mouse_target[, 2]))
 }
