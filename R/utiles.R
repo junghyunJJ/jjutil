a59ede666c6bc47cba16d17fe8f8efd99a139ab4
junghyunjj <swordbreadjj@gmial.com>
2024-05-12 00:27:12 -0700

    update

--- R/utiles.R
+++ R/utiles.R
@@ -20,5 +20,38 @@ human_to_mouse <- function(human_ids, input = "ENSEMBL", target = "SYMBOL") {
   return(data.frame(human_input = human_ids, mapped, mouse_target = mouse_target[, 2]))
 }
 
+# jjutil::mouse_to_human(mouse_ids = c("Mapk10", "Syt5", "Kank4", "Crb2"), input = "SYMBOL", target = "ENSEMBL")
+# jjutil::human_to_mouse(c("ENSG00000109339", "ENSG00000129990", "ENSG00000132854", "ENSG00000148204"))
 
 
+hENSEMBL_to_mSYMBOL <- function(human_ids) {
+
+  # human_geneid <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db, human_ids, "ENTREZID", input)
+  anno_human <- inner_join(
+    AnnotationDbi::toTable(org.Hs.eg.db::org.Hs.egSYMBOL),
+    AnnotationDbi::toTable(org.Hs.eg.db::org.Hs.egENSEMBL),
+    by = "gene_id"
+  )
+
+  human_geneid <- anno_human %>%
+    filter(ensembl_id %in% human_ids) %>%
+    pull(gene_id) %>%
+    unique
+
+  mapped <- AnnotationDbi::select(Orthology.eg.db::Orthology.eg.db, human_geneid, "Mus_musculus", "Homo_sapiens")
+  mapped <- mapped[!is.na(mapped$Mus_musculus), ]
+
+  # mouse_target <- AnnotationDbi::select(org.Mm.eg.db::org.Mm.eg.db, as.character(mapped[, 2]), target, "ENTREZID")
+  anno_mouse <- inner_join(
+    AnnotationDbi::toTable(org.Mm.eg.db::org.Mm.egSYMBOL),
+    AnnotationDbi::toTable(org.Mm.eg.db::org.Mm.egENSEMBL),
+    by = "gene_id"
+  )
+
+  mouse_target <- anno_mouse %>%
+    filter(gene_id %in% mapped$Mus_musculus) %>%
+    pull(symbol) %>%
+    unique
+
+  return(list(human_input = human_ids, mapped, mouse_target = mouse_target))
+}
