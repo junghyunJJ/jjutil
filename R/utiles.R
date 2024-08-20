c9755c78ebe1112ade9754c25afe09f7d5a3029a
junghyunjj <swordbreadjj@gmial.com>
2024-08-20 14:35:34 -0700

    update "summry_gprofiler2" function

--- R/utiles.R
+++ R/utiles.R
@@ -10,7 +10,9 @@ summry_gprofiler2 <- function(enrich, plot = FALSE) {
     mutate(n_GeneRatio = intersection_size / query_size)
 
   res_enrich <- raw_res_enrich %>%
-    select(query, p_value, source, term_name, term_id, GeneRatio, BgRatio, n_GeneRatio, intersection_size, intersection)
+    select(query, p_value, source, term_name, term_id, GeneRatio, BgRatio, n_GeneRatio, intersection_size, intersection) %>%
+    mutate(term = paste0(term_name, "(", term_id, ")")) %>%
+    select(term, query, p_value, source, everything())
 
   if (plot) {
     gprofiler2::gostplot(enrich, capped = TRUE, interactive = TRUE)
